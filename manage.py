d481a2ee9d92f3bedc1336b950881731f7cf13a1
ymang <ymangz@foxmail.com>
2020-04-15 11:17:49 +0800

    增加manage.py启动逻辑

--- manage.py
+++ manage.py
@@ -12,9 +12,10 @@ def setup():
     :return:
     """
     parser = argparse.ArgumentParser(description="接口测试启动程序")
+    parser.add_argument('mode', help="脚本模式,s:starttest")
     parser.add_argument('-p', '--product', dest='product', nargs='*', type=str, help='执行测试的一级模块名.')
     parser.add_argument('-m', '--module', dest='module', nargs='*', type=str, help='执行测试的模块名.')
-    parser.add_argument('-l', '--level', dest='level', nargs='*', choices=[1, 2, 3], type=int,
+    parser.add_argument('-l', '--level', dest='level', nargs='*', choices=["1", "2", "3"], type=str,
                         help='执行的用例等级在1 2 3中选择')
     parser.add_argument('-a', '--apipath', dest='api_path', nargs='*', type=str, help='执行的api路径,例如/path/to/api')
 
@@ -32,5 +33,6 @@ RUN_LEVEL = setup().level
 RUN_PATH = setup().api_path
 
 if __name__ == '__main__':
-    pytest.main(["-vv", "-s", APITESTCASE_HOME, "--color=no", f"--alluredir={REPORT_XML_DIR}"])
-    os.system(f"allure generate  {REPORT_XML_DIR} -o {REPORT_HTML_DIR} --clean")
+    if setup().mode in ["s", "starttest"]:
+        pytest.main(["-vv", "-s", APITESTCASE_HOME, "--color=no", f"--alluredir={REPORT_XML_DIR}"])
+        os.system(f"allure generate  {REPORT_XML_DIR} -o {REPORT_HTML_DIR} --clean")
